cc7c1eb0e0ac67e88339d4bae93cbff4a26b8934
NikitaProts <57028143+NikitaProts@users.noreply.github.com>
2020-05-04 11:17:22 +0300

    Update test_pytest.py

--- tests/test_pytest.py
+++ tests/test_pytest.py
@@ -3,11 +3,12 @@ import sys
 import random
 import index
 
+#Простые текстики, которые смог придумать
 
-def test_letter_first():
+def test_first_word():
     words = ["skillfactory", "testing", "blackbox", "pytest", "unittest", "coverage"]
     secret_word = random.choice(words)
     assert secret_word in words
 
-def test_letter_second():
+def test_second_word():
     assert str(int()) not in index.secret_word
